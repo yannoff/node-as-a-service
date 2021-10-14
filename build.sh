5505391bd3cbf272e6a2ee30ea9cb139cc152f43
Yannoff <yannoff.it@gmail.com>
2021-10-14 19:15:05 +0200

    [builder] Cleanup images after push

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ build_and_push(){
     local version=$1 tag=$2
     [ -z ${tag} ] && tag=${version}
     docker pull node:${version}-alpine
-    docker build -t ${image}:${tag} --build-arg VERSION=${version} docker/ && docker push ${image}:${tag}
+    docker build -t ${image}:${tag} --build-arg VERSION=${version} docker/ && \
+    docker push ${image}:${tag} && \
+    docker rmi ${image}:${tag} node:${version}-alpine
     printf "Building image \033[01m%s:%s\033[00m ...\033[01;32mOK\033[00m\n" "${image}" "$tag"
 }
 
